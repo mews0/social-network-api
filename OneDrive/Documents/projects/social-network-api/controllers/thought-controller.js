9bfe1c95b54a8df7544b7d6faf3f7def147f3023
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-04 16:15:52 -0500

    Create the thought controller

---
+++ OneDrive/Documents/projects/social-network-api/controllers/thought-controller.js
@@ -0,0 +1,77 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // GET all thoughts
+  getAllThought() {
+    Thought.find({})
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // GET a single thought by its _id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then(dbThoughtData => {
+        if(!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!'});
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // POST to add a new thought
+  addThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { thoughts: _id } }, // use the $push method to add the thought's _id to the specific user we want to update
+          { new: true }
+        );
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!'});
+          return;
+        }
+        res.json(dbUserData)
+      })
+      .catch(err => res.json(err));
+  },
+
+  // PUT to update a thought by its _id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // DELETE to remove a thought by its _id
+  removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+};
+
+module.exports = thoughtController;

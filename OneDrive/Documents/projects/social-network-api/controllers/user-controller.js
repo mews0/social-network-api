ffd841e5cac2ce6f4a79479ae9e6225915e6ba9c
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-05 15:54:51 -0500

    Update friend queries

--- OneDrive/Documents/projects/social-network-api/controllers/user-controller.js
+++ OneDrive/Documents/projects/social-network-api/controllers/user-controller.js
@@ -69,7 +69,7 @@ const userController = {
   addFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
-      { $push: { friends: { friendId: params.friendId } } },
+      { $push: { friends: params.friendId } },
       { new: true, runValidators: true }
     )
       .then(dbUserData => {
@@ -86,7 +86,7 @@ const userController = {
   removeFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
-      { $pull: { friends: { friendId: params.friendId } } },
+      { $pull: { friends: params.friendId } },
       { new: true }
     )
       .then(dbUserData => res.json(dbUserData))

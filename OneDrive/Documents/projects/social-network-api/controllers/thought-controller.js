1277a5027803582170afcd0076e0e607868aa55c
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-05 12:36:16 -0500

    Update the thought controller with reaction queries

--- OneDrive/Documents/projects/social-network-api/controllers/thought-controller.js
+++ OneDrive/Documents/projects/social-network-api/controllers/thought-controller.js
@@ -72,6 +72,34 @@ const thoughtController = {
         res.json(dbThoughtData);
       })
       .catch(err => res.status(400).json(err));
+  },
+
+  // POST to add a new reaction to a thought
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // DELETE to remove a reaction from a thought
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
   }
 };
 

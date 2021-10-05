d059df7999fa869792718d93b61cb30c64751cb4
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-05 14:51:49 -0500

    Update the user controller with friend queries

--- OneDrive/Documents/projects/social-network-api/controllers/user-controller.js
+++ OneDrive/Documents/projects/social-network-api/controllers/user-controller.js
@@ -11,9 +11,14 @@ const userController = {
       });
   },
 
-  // GET a single user by its _id (*** and populated thought and friend data ***)
+  // GET a single user by its _id and populated thought and friend data
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
@@ -58,6 +63,34 @@ const userController = {
         res.json(dbUserData);
       })
       .catch(err => res.status(400).json(err));
+  },
+
+  // POST to add a new friend to a user's friend list
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // DELETE to remove a friend from a user's friend list
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: { friendId: params.friendId } } },
+      { new: true }
+    )
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
   }
 };
 
